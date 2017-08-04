4d49a1a03bcd2ecdae920c8857399e567103f4f4
Andrew Hegman <arh426@gmail.com>
2017-08-03 20:58:51 -0500

    Its almost complete. Doesn't look like it is quite working though. After 1000 epochs, the output is only 0.43214807223122 (it hasn't changed in that long...) but it should be 0. Maybe I just don't understand NeuralNets well enough? Need to train with more inputs for sure.

--- NeuralNet_Test.py
+++ NeuralNet_Test.py
@@ -12,39 +12,45 @@ class NeuralNetwork:
 
         self.TestW1 = np.matrix('0.8 0.4 0.3; 0.2 0.9 0.5')
         self.TestW2 = np.matrix('0.3; 0.5; 0.9')
-
         self.Output = None
         self.OutputLayerInputs = None
         self.HiddenLayerOutputs = None
         self.HiddenLayerInputs = None
+        self.Inputs = None
+        self.SigmoidPrimeMatrix = np.frompyfunc(self.SigmoidPrime, 1, 1)
+        self.SigmoidMatrix = np.frompyfunc(self.Sigmoid, 1, 1)
+
+    def Reset(self):
+        self.Output = None
+        self.OutputLayerInputs = None
+        self.HiddenLayerOutputs = None
+        self.HiddenLayerInputs = None
+        self.Inputs = None
 
     def Sigmoid(self, x):
         return 1/(1 + np.exp(-x))
 
     def SigmoidPrime(self, x):
-        if(type(x) is np.matrixlib.defmatrix.matrix):
-            return np.apply_along_axis(self.Sigmoid, 0, x)
-        else:
-            return(self.Sigmoid(x) * (1-self.Sigmoid(x)))
+        return(self.Sigmoid(x) * (1-self.Sigmoid(x)))
 
     def Forward(self, Input1, Input2):
-        Inputs = np.matrix([[Input1,Input2]])
-        self.HiddenLayerInputs = np.dot(Inputs, self.TestW1)
-        self.HiddenLayerOutputs = self.Sigmoid(self.HiddenLayerInputs)
+        self.Inputs = np.matrix([[Input1,Input2]])
+        self.HiddenLayerInputs = np.dot(self.Inputs, self.TestW1)
+        self.HiddenLayerOutputs = self.SigmoidMatrix(self.HiddenLayerInputs)
         self.OutputLayerInputs = np.dot(self.HiddenLayerOutputs, self.TestW2)
-        self.Output = self.Sigmoid(self.OutputLayerInputs)
-
+        self.Output = self.SigmoidMatrix(self.OutputLayerInputs)
 
     def Backward(self, target):
-        DeltaOutputSum = self.SigmoidPrime(self.OutputLayerInputs) * (target - self.Output)
-        DeltaHiddenSum = (DeltaOutputSum / self.TestW2) * self.SigmoidPrime(self.HiddenLayerInputs)
-        self.TestW2 = DeltaOutputSum.item(0) / self.HiddenLayerOutputs
-
-
-        print(self.TestW2)
-
+        DeltaOutputSum = self.SigmoidPrimeMatrix(self.OutputLayerInputs) * (target - self.Output)
+        DeltaHiddenSum = np.multiply(np.transpose((DeltaOutputSum / self.TestW2)), self.SigmoidPrimeMatrix(self.HiddenLayerInputs))
+        self.TestW2 = np.transpose(np.divide(DeltaOutputSum.item(0), self.HiddenLayerOutputs))
+        self.TestW1 = self.TestW1 + np.divide(DeltaHiddenSum, np.transpose(self.Inputs))
 
 Test = NeuralNetwork()
-Test.Forward(1, 1)
-Test.Backward(0)
+
+for i in range(0, 10000000000):
+    Test.Forward(1, 1)
+    print(Test.Output)
+    Test.Backward(0)
+
 
